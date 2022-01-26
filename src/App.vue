ca27dd2b0ef9f07d64a0cf1840b2f52114ecfa70
lipei <lipei@ibike668.com>
2022-01-26 14:11:36 +0800

    ts基本用法

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
+    <h1>学习demo</h1>
     <!-- 根级路由出口 -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
-<style lang="scss" scoped>
-</style>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      a: 2,
+      b: 'cc'
+    }
+  },
+  methods: {
+    test () {
+      console.log(this.a)
+    }
+  }
+})
+</script>
+<style lang="scss" scoped></style>
