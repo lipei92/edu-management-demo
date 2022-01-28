666ff9316af747a5066dbdfc22fb0099bb8d2d4f
lipei <lipei@ibike668.com>
2022-01-28 16:45:16 +0800

    引入element ui

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <h1>学习demo</h1>
     <!-- 根级路由出口 -->
     <router-view />
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type='primary'>主要按钮</el-button>
+      <el-button type='success'>成功按钮</el-button>
+      <el-button type='info'>信息按钮</el-button>
+      <el-button type='warning'>警告按钮</el-button>
+      <el-button type='danger'>危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 <script lang="ts">
